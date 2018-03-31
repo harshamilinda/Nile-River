933199e0b4cd7a7605722d37c05fe5c27b33a27a
harshamilinda@gmail.com <harshamilinda@gmail.com>
2018-03-31 22:57:30 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -20,8 +20,12 @@ class App extends Component {
         <Person author={this.state.authors[1].name} name=" River in Africa">There are two theories about the age of the integrated Nile. One is that the integrated drainage of the Nile is of young age, 
         and that the Nile basin was formerly broken into series of separate basins, 
         only the most northerly of which fed a river following the present course of the Nile in Egypt and Sudan</Person>
+        <button onClick={this.refreshHandler}>Refresh</button>
       </div>
     );
+  }
+  refreshHandler = () => {
+    console.log('Refresh Handler clicked!');
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,12 +4,17 @@ const person = (props) => {
     return (
         <div>
             <p className='person'>The Nile is a major north-flowing river in northeastern Africa, and is commonly regarded as the longest river in the world,
-            though some sources cite the Amazon River as the longest.<br /><strong>Author:</strong>{props.name} </p>
-            <p className='section'>{props.children}</p>
+            though some sources cite the Amazon River as the longest.<br /><strong>Reference:</strong>{props.name} </p>
+            <p className='section'>{props.children}<br /><button onClick={informationHandler}>Information</button></p>
             <p className='mini-section'><strong>Author: </strong>{props.author}</p>
         </div>
     )
+    
+
 
 }
 
+const informationHandler = () => {
+    console.log('Information Handler clicked !');
+}
 export default person;
