084486b0426048eaec0d7e81fe83fe729bed9d7b
harshamilinda@gmail.com <harshamilinda@gmail.com>
2018-04-01 17:17:56 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -4,21 +4,31 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-    state ={
-      authors: [
-        { name:'Henry Morton Stanley'},
-        { name:'John Hanning Speke'}
-      ]
-    }
-  
-  refreshHandler = ()=>{
+  state = {
+    authors: [
+      { name: 'Henry Morton Stanley' },
+      { name: 'John Hanning Speke' }
+    ]
+  }
+
+  refreshHandler = (newName) => {
     this.setState({
       authors: [
-        { name:'John Hanning Speke'},
-        { name:'Henry Morton Stanley'}
+        { name: 'John Hanning Speke' },
+        { name: newName }
       ]
     })
   }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      authors: [
+        { name: 'Luther Lecx' },
+        { name: event.target.value}
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -26,14 +36,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Hi, Harsha</h1>
         </header>
-        <Person click={this.refreshHandler} author={this.state.authors[1].name} name=" River in Africa">There are two theories about the age of the integrated Nile. One is that the integrated drainage of the Nile is of young age, 
-        and that the Nile basin was formerly broken into series of separate basins, 
+        <Person click={this.refreshHandler.bind(this,'Michael Llewelyn Davies')} 
+                author={this.state.authors[1].name} 
+                textChange={this.nameChangeHandler}
+                name=" River in Africa">There are two theories about the age of the integrated Nile. One is that the integrated drainage of the Nile is of young age,
+        and that the Nile basin was formerly broken into series of separate basins,
         only the most northerly of which fed a river following the present course of the Nile in Egypt and Sudan</Person>
-        <button onClick={this.refreshHandler}>Refresh</button>
+        <div className='Buttton-Container'><button className='mini-button' 
+                onClick={this.refreshHandler.bind(this, 'John Hanning Speke')}>Refresh</button></div>
+        
       </div>
     );
   }
- 
+
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,9 +5,9 @@ const person = (props) => {
         <div>
             <p className='person'>The Nile is a major north-flowing river in northeastern Africa, and is commonly regarded as the longest river in the world,
             though some sources cite the Amazon River as the longest.<br /><strong>Reference:</strong>{props.name} <button onClick={props.click}>Info</button> </p>
-            <p className='section'>{props.children}<br /></p>
+            <p className='section'>{props.children}<br /><input type="text" onChange={props.textChange} value={props.author} /></p>
             <p className='mini-section'><strong>Author: </strong>{props.author}</p>
-            
+
         </div>
     )
 }
